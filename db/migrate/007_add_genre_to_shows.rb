6cec3d0d0502c177d642e039e4f0a013911742c4
Eden Mesfun <emesfun@gmail.com>
2018-03-19 16:37:58 +0000

    automatically backed up by learn

--- db/migrate/007_add_genre_to_shows.rb
+++ db/migrate/007_add_genre_to_shows.rb
@@ -1,6 +1,6 @@
 class AddGenreToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :genre, :string
-    add_column :shows, :season, :string
+    add_column :shows, :call_letters, :string
   end
 end
