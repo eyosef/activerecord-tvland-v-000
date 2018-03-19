63fce6e4ce6c0ebd1cbb48557815ac8be36b4f94
Eden Mesfun <emesfun@gmail.com>
2018-03-19 17:10:49 +0000

    automatically backed up by learn

--- db/migrate/009_add_catchphrase_to_characters.rb
+++ db/migrate/009_add_catchphrase_to_characters.rb
@@ -1,5 +1,6 @@
-class AddCatchPhraseToCharacters < ActiveRecord::Migration[4.2]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :catchphrase, :string
   end
+
 end
