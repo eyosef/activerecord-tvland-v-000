b6198a737888beceb2067c5495904c958542b900
Eden Mesfun <emesfun@gmail.com>
2018-03-19 19:04:05 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,10 +6,4 @@ class Show < ActiveRecord::Base
     the_asso = Network.create(asso)
     self.network = the_asso
   end
-
-  # def build_show(show)
-  #   the_show = Show.create(show)
-  #   self.show = the_show
-  # end
-
 end
