881ce6cbb2a600996b51a2db637b66c71c8d1c6b
Eden Mesfun <emesfun@gmail.com>
2018-03-19 17:18:31 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,6 +8,10 @@ class Character < ActiveRecord::Base
 
   def build_show(show)
     self.show = show[:name]
+
+    # jules_cobb = Character.new(:name => "Jules Cobb")
+    # jules_cobb.build_show(:name => "Cougar Town")
+    # expect(jules_cobb.show.name).to eq("Cougar Town")
   end
 
 end
