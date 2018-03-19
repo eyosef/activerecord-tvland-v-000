18a83b07656639290466f693a847aaa8ea507e55
Eden Mesfun <emesfun@gmail.com>
2018-03-19 16:57:12 +0000

    automatically backed up by learn

--- db/migrate/008_add_actor_id_to_characters.rb
+++ db/migrate/008_add_actor_id_to_characters.rb
@@ -1,6 +1,6 @@
 class AddActorIdToCharacters < ActiveRecord::Migration[4.2]
   def change
-    add_column :characters, :actor_id, :string
+    add_column :characters, :actor_id, :integer
     add_column :shows, :call_letters, :string
   end
 end
